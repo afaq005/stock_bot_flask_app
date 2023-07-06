872b8bd818c0ddd554af21aeebc58c8d21f483b3
Afaq <53523131+afaq005@users.noreply.github.com>
2023-07-06 15:38:59 +0900

    Update stock_bot.py

--- stock_bot.py
+++ stock_bot.py
@@ -1,14 +1,15 @@
 from flask import Flask, jsonify
 import gdown
 app = Flask(__name__)
-
+file_url = "https://drive.google.com/uc?export=download&id=1o8TAPB2AvaIvn9_4TcA39ukI48j0uYR5"
+output_file = "tradingBot.ipynb"
 @app.route('/run-colab')
 def run_colab():
     print("inside flask file")
-    gdown.download('https://drive.google.com/file/d/1o8TAPB2AvaIvn9_4TcA39ukI48j0uYR5/view?usp=drive_link', 'colab.ipynb', quiet=True)
+    gdown.download(file_url, output_file, quiet=True)
     return jsonify(message='colab notebook ran successfully')
 
 
 if __name__ == '__main__':
     print("Runing main file")
-    app.run()')
+    app.run()

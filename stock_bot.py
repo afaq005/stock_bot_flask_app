fb8d8af4c4f55242ece53d571d3cfdc4d9574f05
Afaq <53523131+afaq005@users.noreply.github.com>
2023-07-06 11:09:28 +0900

    Update stock_bot.py

--- stock_bot.py
+++ stock_bot.py
@@ -4,5 +4,11 @@ app = Flask(__name__)
 
 @app.route('/run-colab')
 def run_colab():
+    print("inside flask file")
     gdown.download('https://drive.google.com/file/d/1o8TAPB2AvaIvn9_4TcA39ukI48j0uYR5/view?usp=drive_link', 'colab.ipynb', quiet=True)
     return jsonify(message='colab notebook ran successfully')
+
+
+if __name__ == '__main__':
+    print("Runing main file")
+    app.run()')

5fbc9efafa42b74eb39df5de74a3a72bcdfaf620
Afaq <53523131+afaq005@users.noreply.github.com>
2023-07-07 10:24:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+import subprocess
 from flask import Flask, jsonify
 import gdown
-from nbconvert import NotebookExecutor
+
 app = Flask(__name__)
+
 file_url = "https://drive.google.com/uc?export=download&id=1o8TAPB2AvaIvn9_4TcA39ukI48j0uYR5"
 output_file = "tradingBot.ipynb"
+output_format = "html"
 @app.route('/run-colab')
 def run_colab():
     print("inside flask file")
     gdown.download(file_url, output_file, quiet=False)
-    executor = NotebookExecutor(notebook=output_file)
-    executor.run_notebook()
-    return jsonify(message='colab notebook ran successfully')
-
+    
+    try:
+        subprocess.run(["jupyter", "nbconvert", "--execute", "--to", output_format, output_file], check=True)
+        return jsonify(message='Colab notebook ran successfully')
+    except subprocess.CalledProcessError:
+        return jsonify(message='Failed to run Colab notebook')
 
 if __name__ == '__main__':
-    print("Runing main file")
-    
+    print("Running main file")
     app.run()
-    jsonify(message='Hello')
